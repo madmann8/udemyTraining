d1d9a20cc8eb8a7d7ebcb775fa990182b81bca1f
madmann8 <madmann686@gmail.com>
2016-03-12 10:57:04 -0500

    with " package main"

---
+++ forloop/condition/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main (){
+	i:=3
+	for i<1000 {
+		i++
+	fmt.Println(i)
+	}
+}
---
+++ forloop/nested/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 1000; j++ {
+			fmt.Println(i, " - ", j)
+
+		}
+	}
+}
+
+
