dc41ad660fe96decd6feb5d986e4c5c730d75bc2
madmann8 <madmann686@gmail.com>
2016-03-12 20:32:47 -0500

    remainder claculator

---
+++ exercisesPt1/smallandlargenumbers/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var largeNumber int
+	var smallNumber int
+	fmt.Print("Enter a large number: ")
+	fmt.Scan(&largeNumber)
+	fmt.Print("Enter a small number: ")
+	fmt.Scan (&smallNumber)
+	remainder := largeNumber%smallNumber
+	fmt.Println("The remainder of the large number divided by the large number is ", remainder)
+}
