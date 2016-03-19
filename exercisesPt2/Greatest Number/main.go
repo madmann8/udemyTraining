35c7b5cdbaecd173167a356391dd87b22150d581
madmann8 <madmann686@gmail.com>
2016-03-18 20:59:37 -0400

    half of a number

---
+++ exercisesPt2/Greatest Number/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func greatest(n...int)int {
+	largest:=0
+	l:= len(n)
+	for i:=0;i>l;i++{
+		if i == 0 {
+			largest = n(i)
+			continue
+		}
+		if i> largest {
+			largest= n(i)
+			continue
+		}
+		return n
+	}
+
+}
+
+func main () {
+	fmt.Println(greatest(1,2,3,4,5))
+}
