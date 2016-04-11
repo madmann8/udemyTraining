0431d09f372774ea287aa62d35e9f4ffe24074db
madmann8 <madmann686@gmail.com>
2016-04-11 17:26:42 -0400

    channels practice pt3

--- channels/factorials.go
+++ channels/factorials.go
@@ -1,3 +1,23 @@
 package main
 
+import "fmt"
 
+func main() {
+	n:= fact(20)
+	for r:= range(n) {
+		fmt.Println(r)
+	}
+}
+
+func fact (n int)chan int {
+	out:=make(chan int)
+	go func(){
+		total:= 1
+		for i := n; i > 0; i-- {
+			total *= i
+		}
+		out <- total
+		close(out)
+	}()
+	return out
+}
