8e85de9f330676e4d43af47e9bc3c2f6fcfbc409
madmann8 <madmann686@gmail.com>
2016-04-12 19:24:52 -0400

    channels practice

--- channels/factorials.go
+++ channels/factorials.go
@@ -1,23 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var (
+	s1 rand.Source
+	r1 *rand.Rand
+)
+
+type Payload struct {
+	Timestamp time.Time
+	Work      int
+}
+
+func init() {
+	s1 = rand.NewSource(time.Now().UnixNano())
+	r1 = rand.New(s1)
+}
 
 func main() {
-	n:= fact(20)
-	for r:= range(n) {
-		fmt.Println(r)
+
+	work := make(chan Payload)
+	done := make(chan bool)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			randDelay()
+			work <- Payload{
+				Timestamp: time.Now(),
+				Work:      i,
+			}
+		}
+		done <- true
+	}()
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			randDelay()
+			work <- Payload{
+				Timestamp: time.Now(),
+				Work:      i,
+			}
+		}
+		done <- true
+	}()
+
+	go func() {
+
+		<-done
+
+		<-done
+
+		close(work)
+	}()
+
+	for n := range work {
+		elapsed := time.Since(n.Timestamp)
+		fmt.Printf("%d finished in %q time\n", n.Work, elapsed)
 	}
 }
 
-func fact (n int)chan int {
-	out:=make(chan int)
-	go func(){
-		total:= 1
-		for i := n; i > 0; i-- {
-			total *= i
-		}
-		out <- total
-		close(out)
-	}()
-	return out
+func randDelay() {
+
+	randomValue := r1.Intn(1000)
+	time.Sleep(time.Millisecond * time.Duration(randomValue))
 }
